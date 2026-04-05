0e8abd78306c222ab72826a5d56b3bbb040b4aeb
h13i <hori9180197@gmail.com>
2026-04-05 22:47:20 +0900

    remove: VercelアダプターとcompressHTMLオプションをastro.config.mjsから削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: false,
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
